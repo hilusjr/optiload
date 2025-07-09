0ef985b7ddfe523e1fbaecc70fd113108dc69ad8
hilusjr <pietrzef@gmail.com>
2025-07-10 00:36:28 +0200

    update

--- script/results.js
+++ script/results.js
@@ -3,6 +3,8 @@ const successfullyPlacedList = document.querySelector(
   '.successfully-placed-list'
 )
 
+const failedToPlaceList = document.querySelector('.failed-list')
+
 const packages = JSON.parse(sessionStorage.getItem('packages'))
 const successfullyPlacedPackages = JSON.parse(
   sessionStorage.getItem('successfullyPlacedPackages')
@@ -14,9 +16,45 @@ openRender.addEventListener('click', () => {
 })
 
 window.addEventListener('load', () => {
+  renderSummary()
+  renderList('success')
+  renderList('failed')
+})
+
+function renderSummary() {
+  const packagesLabel = document.querySelector('.packages-label')
+  const successfullyLabel = document.querySelector('.successfully-label')
+  const failedLabel = document.querySelector('.failed-label')
+  const weightLabel = document.querySelector('.weight-label')
+  const volumeLabel = document.querySelector('.volume-label')
+
+  let weight = 0
+  let volume = 0
   successfullyPlacedPackages.forEach(pkg => {
+    weight += pkg.weight
+    volume +=
+      pkg.dimensions.width * pkg.dimensions.length * pkg.dimensions.height
+  })
+
+  packagesLabel.textContent = `📦 Packages before optimization: ${packages.length}`
+  successfullyLabel.textContent = `✅ Packages placed onto a vehicle: ${successfullyPlacedPackages.length}`
+  failedLabel.textContent = `❌ Packages failed to be loaded: ${failedToPlace.length}`
+  weightLabel.textContent = `⚖️ Weight of the load: ${weight} kg`
+  volumeLabel.textContent = `📐 Volume of occupied cargo space: ${volume} cm³ / ${
+    volume / 1000000
+  } m³`
+}
+
+function renderList(type) {
+  const packagesArray =
+    type === 'success' ? successfullyPlacedPackages : failedToPlace
+
+  const listElement =
+    type === 'success' ? successfullyPlacedList : failedToPlaceList
+
+  packagesArray.forEach(pkg => {
     const pkgElement = document.createElement('div')
-    pkgElement.classList.add('package-item')
+    pkgElement.classList.add('results-package-item')
 
     const packageLabel = document.createElement('div')
     packageLabel.textContent = `Package #${pkg.id}`
@@ -35,12 +73,16 @@ window.addEventListener('load', () => {
     const distanceLabel = document.createElement('div')
     distanceLabel.textContent = `${pkg.distance} km`
 
+    const objectiveValueLabel = document.createElement('div')
+    objectiveValueLabel.textContent = `${pkg.objectiveValue}`
+
     pkgElement.appendChild(packageLabel)
     pkgElement.appendChild(dimensionsLabel)
     pkgElement.appendChild(weightLabel)
     pkgElement.appendChild(daysLeftLabel)
     pkgElement.appendChild(distanceLabel)
+    pkgElement.appendChild(objectiveValueLabel)
 
-    successfullyPlacedList.appendChild(pkgElement)
+    listElement.appendChild(pkgElement)
   })
-})
+}
